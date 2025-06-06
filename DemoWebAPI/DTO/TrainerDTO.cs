ceeda59f9defc2bb8cf2677507806e565f6daba8
= <ledouppe.s@gmail.com>
2025-06-06 13:52:29 +0200

    second commit

---
+++ DemoWebAPI/DTO/TrainerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoWebAPI.DTO
+{
+    public class TrainerDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool OnVacation { get; set; }
+    }
+}
